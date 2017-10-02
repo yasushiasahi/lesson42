184913ddedb61d1aad69edbb76933ecde12e7427
Yasushi Asahi <asahi1600@gmail.com>
2017-10-02 16:06:23 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/yasushi/asahi/lesson4/MainActivity.java
@@ -0,0 +1,24 @@
+package jp.techacademy.taro.kirameki.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d("UI_PARTS", "ボタンをタップしました");
+    }
+}
